762d0f790b0a6d74eb0e290ef844571d7f9f633a
GUSTAVO DE PAULA SILVA <25132581@CFP127ALUNO.LOCAL>
2025-09-30 16:57:48 -0300

    não terminadado

--- app.js
+++ app.js
@@ -13,3 +13,59 @@
  *                  Request     -> Recebe os dados
  *                  Response    -> Envia os dados na API
 ************************************************************************************************************************************************/
+//import das dependencias 
+const express       = require('express')
+const cors          = require('cors')
+const bodyParser    = require('body-parser')
+
+//Import do arquivo de funções
+const dados = require('./modulo/funcoes.js')
+
+//defini a porta padrão da API, se for em um servidor de nuvem não temos acesso a porta em execução local podemos definir uma porta
+const PORT= process.PORT || 8080
+
+//Instacia na classe do express
+const app = express()
+
+// Configurações do CORS
+app.use((request, response, next) =>{
+    response.header('Access-Control-Allow-Origin', '*') //IP de Origem
+    response.header('Access-Control-Allow-Methods', 'GET') // Metodos (Verbos) do protocolo HTTP
+    
+    app.use(cors())
+    next()// Ler os proximos EndPoints
+})
+
+
+//EndPoints
+app.get('/v1/allUsers', function(request, response){
+    let users = dados.getAllUsers()
+    response.json(users)
+})
+
+app.get('/v1/User/:number', function(request, response){
+    let number = request.params.number
+    let user = dados.getUserData(number)
+    response.json(user)
+})
+
+app.get('/v1/message/', function(request, response){
+    let numberUser = request.query.numberUser
+    let numberContact = request.query.numberContact
+    let filtered = dados.getUserMesssagesBycontacts(numberUser,numberContact)
+    response.json(filtered)
+})
+
+
+
+app.get('/v1/filter/', function(request, response){
+    let number = request.query.numberUser
+    let word = request.query.keyWord
+    let filtered = dados.filterBykeyWord(number, word)
+    response.json(filtered)
+})
+
+
+app.listen(PORT, function(){
+    console.log("teste")
+})
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -19,7 +19,11 @@ const getAllUsers = function(){
         message.users.push(item)
     })
     // Adiciona um elemneto existente no JSON
-    console.log(message)
+    if(message.users){
+        return message //Resultado Verdadeiro da API 200
+        }else{
+            return MESSAGE_ERROR //Resultado Falso da API 500
+        }
 }
 const getUserData = function(number){
     let message = {status: true, statusCode: 200, development:"Gustavo de Paula Silva",}
@@ -29,7 +33,11 @@ const getUserData = function(number){
 
     message.contato = numberEncontrado
 
-    console.log(message)
+    if(message.contato){
+        return message //Resultado Verdadeiro da API 200
+        }else{
+            return MESSAGE_ERROR //Resultado Falso da API 500
+        }
 }
 const getUserContacts = function(number){
     let message = {status: true, statusCode: 200, development:"Gustavo de Paula Silva",}
@@ -40,7 +48,11 @@ const getUserContacts = function(number){
     message.contato = numberEncontrado.contacts
     delete message.contato.contacts
 
-    console.log(message)
+    if(message.contato){
+        return message //Resultado Verdadeiro da API 200
+        }else{
+            return MESSAGE_ERROR //Resultado Falso da API 500
+        }
 }
 const getUserMesssages = function(number){
     let message = {status: true, statusCode: 200, development:"Gustavo de Paula Silva",}
@@ -52,21 +64,72 @@ const getUserMesssages = function(number){
     message.contato = numberEncontrado.contacts
 
 
-    console.log(message)
+    if(message.contato){
+        return message //Resultado Verdadeiro da API 200
+        }else{
+            return MESSAGE_ERROR //Resultado Falso da API 500
+        }
 }
 const getUserMesssagesBycontacts = function(numberUser,numberContact){
 
-    let message = {status: true, statusCode: 200, development:"Gustavo de Paula Silva",}
+    let message = {status: true, statusCode: 200, development:"Gustavo de Paula Silva", contato: []}
 
     const numberUserEscolhido = numberUser
     const numberUserEncontrado = dados.contatos['whats-users'].find(contato => contato.number === String(numberUserEscolhido))
 
+    const numberContactEscolhido = numberContact
 
+    message.contato.push(numberUserEncontrado)
 
-    console.log(message)
+    numberUserEncontrado.contacts.forEach(function(item){
+        if(item.number == String(numberContactEscolhido)){
+            message.contato.contact = item
+        }
+    })
+    delete numberUserEncontrado.contacts
+
+    if(numberUserEncontrado.contacts){
+        return message //Resultado Verdadeiro da API 200
+        }else{
+            return MESSAGE_ERROR //Resultado Falso da API 500
+        }
 }
+const filterBykeyWord = function(numberUser, keyWord){
+    let message = {status: true, statusCode: 200, development:"Gustavo de Paula Silva", contato: []}
+
+    const numberUserEscolhido = numberUser
+    const numberUserEncontrado = dados.contatos['whats-users'].find(contato => contato.number === String(numberUserEscolhido))
+
+    numberUserEncontrado.contacts.forEach(function(item){
+
+        item.messages.forEach(function(item){
+            if(item.content.includes(keyWord)){
+                message.contato.push(item)
+                console.log(message)
+            }
+        })
+
+    })
+
+    if(message.contato){
+        return message //Resultado Verdadeiro da API 200
+        }else{
+            return MESSAGE_ERROR //Resultado Falso da API 500
+        }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserData,
+    getUserContacts,
+    getUserMesssages,
+    getUserMesssagesBycontacts,
+    filterBykeyWord
+}
+
 // getAllUsers()
 // getUserData(11987876567)
 // getUserContacts(11987876567)
 // getUserMesssages(11987876567)
-getUserMesssagesBycontacts(11987876567, 26999999963)
+// getUserMesssagesBycontacts(11987876567, 26999999963)
+// filterBykeyWord(11987876567,"Leonid")
